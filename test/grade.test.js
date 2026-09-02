01b64c8a1f4cf7c3c369e96c056c314db7e5aec1
Somya Barolia <somya.barolia@gmail.com>
2026-09-02 21:37:23 +0300

    added comments in test file

--- test/grade.test.js
+++ test/grade.test.js
@@ -3,32 +3,38 @@ const assert = require("node:assert");
 
 const { getGrade } = require("../script");
 
+//Test Cases for Grade A
 test("Grade A", () => {
     assert.strictEqual(getGrade(95), "A");
     assert.strictEqual(getGrade(90), "A");
 })
 
+//Test Cases for Grade B
 test("Grade B", () => {
     assert.strictEqual(getGrade(89), "B");
     assert.strictEqual(getGrade(80), "B");
 
 })
 
+//Test Cases for Grade C
 test("Grade C", () => {
     assert.strictEqual(getGrade(79), "C");
     assert.strictEqual(getGrade(70), "C");
 })
 
+//Test Cases for Grade D
 test("Grade D", () => {
     assert.strictEqual(getGrade(69), "D");
     assert.strictEqual(getGrade(60), "D");
 })
 
+//Test Cases for Grade F
 test("Grade F", () => {
     assert.strictEqual(getGrade(59), "F");
     assert.strictEqual(getGrade(0), "F");
 })
 
+//Test Cases for Invalid Marks
 test("Invalid Marks", () => {
     assert.strictEqual(getGrade(-1), "Invalid marks");
     assert.strictEqual(getGrade(101), "Invalid marks");
